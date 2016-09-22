e80ed1795355e5f444170b643b320c06743f7d6c
Christopher Olston <olston@google.com>
2016-09-22 10:15:09 -0800

    Eliminate dependency on regexp library. We were only using it in StaticSourceRouter, and we've downgraded that to do sub-string matching. Change: 133976007

--- tensorflow_serving/core/static_source_router_test.cc
+++ tensorflow_serving/core/static_source_router_test.cc
@@ -39,7 +39,7 @@ namespace serving {
 namespace {
 
 TEST(StaticSourceRouterTest, Basic) {
-  const std::vector<string> regexps = {"0th.*", ".*1st"};
+  const std::vector<string> regexps = {"0th", "1st"};
   std::unique_ptr<StaticSourceRouter<StoragePath>> router;
   TF_ASSERT_OK(StaticSourceRouter<string>::Create(regexps, &router));
   std::vector<Source<StoragePath>*> output_ports = router->GetOutputPorts();
