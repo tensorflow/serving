6d35bf4877e438d57cc1e931e92296170fc17351
Christopher Olston <olston@google.com>
2016-10-21 10:48:39 -0800

    Automated rollback of change 136844593 Change: 136856442

---
+++ tensorflow_serving/servables/tensorflow/simple_servers.cc
@@ -0,0 +1,99 @@
+/* Copyright 2016 Google Inc. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#include "tensorflow_serving/servables/tensorflow/simple_servers.h"
+
+#include <algorithm>
+#include <memory>
+#include <string>
+
+#include "tensorflow/contrib/session_bundle/session_bundle.h"
+#include "tensorflow/core/lib/core/errors.h"
+#include "tensorflow/core/lib/core/status.h"
+#include "tensorflow_serving/core/aspired_versions_manager_builder.h"
+#include "tensorflow_serving/core/eager_load_policy.h"
+#include "tensorflow_serving/core/loader.h"
+#include "tensorflow_serving/core/source.h"
+#include "tensorflow_serving/core/source_adapter.h"
+#include "tensorflow_serving/core/storage_path.h"
+#include "tensorflow_serving/core/target.h"
+#include "tensorflow_serving/servables/tensorflow/session_bundle_source_adapter.h"
+#include "tensorflow_serving/servables/tensorflow/session_bundle_source_adapter.pb.h"
+#include "tensorflow_serving/sources/storage_path/file_system_storage_path_source.h"
+#include "tensorflow_serving/sources/storage_path/file_system_storage_path_source.pb.h"
+
+namespace tensorflow {
+namespace serving {
+namespace simple_servers {
+
+namespace {
+
+// Creates a Source<StoragePath> that monitors a filesystem's base_path for new
+// directories. Upon finding these, it provides the target with the new version
+// (a directory). The servable_name param simply allows this source to create
+// all AspiredVersions for the target with the same servable_name.
+Status CreateStoragePathSource(
+    const string& base_path, const string& servable_name,
+    std::unique_ptr<Source<StoragePath>>* path_source) {
+  FileSystemStoragePathSourceConfig config;
+  config.set_servable_name(servable_name);
+  config.set_base_path(base_path);
+  config.set_file_system_poll_wait_seconds(1);
+
+  std::unique_ptr<FileSystemStoragePathSource> file_system_source;
+  TF_RETURN_IF_ERROR(
+      FileSystemStoragePathSource::Create(config, &file_system_source));
+
+  *path_source = std::move(file_system_source);
+  return Status::OK();
+}
+
+// Creates a SessionBundle Source by adapting the underlying
+// FileSystemStoragePathSource. These two are connected in the
+// 'CreateSingleTFModelManagerFromBasePath' method, with the
+// FileSystemStoragePathSource as the Source and the SessionBundleSource as the
+// Target.
+Status CreateSessionBundleSource(
+    std::unique_ptr<SessionBundleSourceAdapter>* source) {
+  SessionBundleSourceAdapterConfig config;
+  TF_RETURN_IF_ERROR(SessionBundleSourceAdapter::Create(config, source));
+
+  return Status::OK();
+}
+
+}  // namespace
+
+Status CreateSingleTFModelManagerFromBasePath(
+    const string& base_path, std::unique_ptr<Manager>* const manager) {
+  std::unique_ptr<SessionBundleSourceAdapter> bundle_source;
+  TF_RETURN_IF_ERROR(CreateSessionBundleSource(&bundle_source));
+  std::unique_ptr<Source<StoragePath>> path_source;
+  TF_RETURN_IF_ERROR(
+      CreateStoragePathSource(base_path, "default", &path_source));
+
+  AspiredVersionsManagerBuilder::Options manager_options;
+  manager_options.aspired_version_policy.reset(new EagerLoadPolicy());
+  std::unique_ptr<AspiredVersionsManagerBuilder> builder;
+  TF_CHECK_OK(AspiredVersionsManagerBuilder::Create(std::move(manager_options),
+                                                    &builder));
+  builder->AddSourceChain(std::move(path_source), std::move(bundle_source));
+  *manager = builder->Build();
+
+  return Status::OK();
+}
+
+}  // namespace simple_servers
+}  // namespace serving
+}  // namespace tensorflow
---
+++ tensorflow_serving/servables/tensorflow/simple_servers_test.cc
@@ -0,0 +1,93 @@
+/* Copyright 2016 Google Inc. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#include "tensorflow_serving/servables/tensorflow/simple_servers.h"
+
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include <gtest/gtest.h>
+#include "tensorflow/contrib/session_bundle/session_bundle.h"
+#include "tensorflow/core/framework/tensor.h"
+#include "tensorflow/core/framework/tensor_testutil.h"
+#include "tensorflow/core/lib/core/status.h"
+#include "tensorflow/core/lib/core/status_test_util.h"
+#include "tensorflow/core/lib/core/stringpiece.h"
+#include "tensorflow/core/lib/io/path.h"
+#include "tensorflow/core/platform/env.h"
+#include "tensorflow/core/public/session.h"
+#include "tensorflow_serving/core/servable_handle.h"
+#include "tensorflow_serving/test_util/test_util.h"
+
+namespace tensorflow {
+namespace serving {
+namespace {
+
+class SimpleServersTest : public ::testing::Test {
+ protected:
+  SimpleServersTest()
+      : test_data_path_(test_util::ContribTestSrcDirPath(
+            "session_bundle/example/half_plus_two")) {}
+
+  // Test that a SessionBundle handles a single request for the half plus two
+  // model properly. The request has size=2, for batching purposes.
+  void TestSingleRequest(const SessionBundle& bundle) {
+    const Tensor input = test::AsTensor<float>({100.0f, 42.0f}, {2});
+    // half plus two: output should be input / 2 + 2.
+    const Tensor expected_output =
+        test::AsTensor<float>({100.0f / 2 + 2, 42.0f / 2 + 2}, {2});
+
+    // Note that "x" and "y" are the actual names of the nodes in the graph.
+    // The saved manifest binds these to "input" and "output" respectively, but
+    // these tests are focused on the raw underlying session without bindings.
+    const std::vector<std::pair<string, Tensor>> inputs = {{"x", input}};
+    const std::vector<string> output_names = {"y"};
+    const std::vector<string> empty_targets;
+    std::vector<Tensor> outputs;
+
+    TF_ASSERT_OK(
+        bundle.session->Run(inputs, output_names, empty_targets, &outputs));
+
+    ASSERT_EQ(1, outputs.size());
+    const auto& single_output = outputs.at(0);
+    test::ExpectTensorEqual<float>(expected_output, single_output);
+  }
+
+  // Test data path, to be initialized to point at an export of half-plus-two.
+  const string test_data_path_;
+};
+
+TEST_F(SimpleServersTest, Basic) {
+  std::unique_ptr<Manager> manager;
+  const Status status = simple_servers::CreateSingleTFModelManagerFromBasePath(
+      test_data_path_, &manager);
+  TF_CHECK_OK(status);
+  // We wait until the manager starts serving the servable.
+  // TODO(b/25545570): Use the waiter api when it's ready.
+  while (manager->ListAvailableServableIds().empty()) {
+    Env::Default()->SleepForMicroseconds(1000);
+  }
+  ServableHandle<SessionBundle> bundle;
+  const Status handle_status =
+      manager->GetServableHandle(ServableRequest::Latest("default"), &bundle);
+  TF_CHECK_OK(handle_status);
+  TestSingleRequest(*bundle);
+}
+
+}  // namespace
+}  // namespace serving
+}  // namespace tensorflow
