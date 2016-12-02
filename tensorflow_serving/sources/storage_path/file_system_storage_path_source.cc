65b12a89d00732481709d2682ccbfe3990c45688
Sukriti Ramesh <sukritiramesh@google.com>
2016-12-02 14:33:21 -0800

    Update file system storage path source logging message to use VLOG. Change: 140894703

--- tensorflow_serving/sources/storage_path/file_system_storage_path_source.cc
+++ tensorflow_serving/sources/storage_path/file_system_storage_path_source.cc
@@ -321,10 +321,10 @@ Status FileSystemStoragePathSource::PollFileSystemAndInvokeCallback() {
     const std::vector<ServableData<StoragePath>>& versions = entry.second;
     for (const ServableData<StoragePath>& version : versions) {
       if (version.status().ok()) {
-        LOG(INFO) << "File-system polling update: Servable:" << version.id()
-                  << "; Servable path: " << version.DataOrDie()
-                  << "; Polling frequency: "
-                  << config_.file_system_poll_wait_seconds();
+        VLOG(1) << "File-system polling update: Servable:" << version.id()
+                << "; Servable path: " << version.DataOrDie()
+                << "; Polling frequency: "
+                << config_.file_system_poll_wait_seconds();
       }
     }
     aspired_versions_callback_(servable, versions);
