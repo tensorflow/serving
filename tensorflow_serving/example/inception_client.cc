91a4d2d34207e51541fae6d7d557dbcba11852ee
zhiliang <zhiliang@robusoft.com.cn>
2017-02-14 11:34:58 +0800

    use the Tensor::FromProto and Tensor::SummarizeValue to output the result make the server_address/model_name/image_file configurable using Tensorflow flag

--- tensorflow_serving/example/inception_client.cc
+++ tensorflow_serving/example/inception_client.cc
@@ -3,7 +3,8 @@
 
 #include <grpc++/create_channel.h>
 #include "tensorflow_serving/apis/prediction_service.grpc.pb.h"
-
+#include "tensorflow/core/framework/tensor.h"
+#include "tensorflow/core/util/command_line_flags.h"
 
 using grpc::Channel;
 using grpc::ClientContext;
@@ -17,13 +18,7 @@ using tensorflow::serving::PredictRequest;
 using tensorflow::serving::PredictResponse;
 using tensorflow::serving::PredictionService;
 
-const std::string SERVING_HOST = "localhost:9000";
-
-const std::string INPUT_FILE_NAME = "/tmp/cropped_panda.jpg";
-const std::string SERVICE_NAME = "inception";
-
-
-typedef ::google::protobuf::Map< ::std::string, ::tensorflow::TensorProto > OutMap;
+typedef google::protobuf::Map< std::string, tensorflow::TensorProto > OutMap;
 
 
 class ServingClient {
@@ -32,17 +27,17 @@ class ServingClient {
       : stub_(PredictionService::NewStub(channel)) {
   }
 
-  std::string callPredict(std::string file_path){
+  std::string callPredict(std::string model_name, std::string file_path){
     PredictRequest predictRequest;
     PredictResponse response;
     ClientContext context;
 
-    predictRequest.mutable_model_spec()->set_name(SERVICE_NAME);
+    predictRequest.mutable_model_spec()->set_name(model_name);
 
-    ::google::protobuf::Map< ::std::string, ::tensorflow::TensorProto >& inputs = 
+    google::protobuf::Map< std::string, tensorflow::TensorProto >& inputs = 
         *predictRequest.mutable_inputs();
 
-    ::tensorflow::TensorProto proto;
+    tensorflow::TensorProto proto;
 
     std::ifstream imageFile(file_path, std::ios::binary);
 
@@ -54,90 +49,50 @@ class ServingClient {
     std::filebuf * pbuf = imageFile.rdbuf();
     long fileSize = pbuf->pubseekoff(0, std::ios::end, std::ios::in);
     
-    char* image = new char[fileSize];
+    char* image = new char[fileSize]();
 
     pbuf->pubseekpos(0, std::ios::in);
     pbuf->sgetn(image, fileSize);
     imageFile.close();
     
 
-    proto.set_dtype(::tensorflow::DataType::DT_STRING);
+    proto.set_dtype(tensorflow::DataType::DT_STRING);
     proto.add_string_val(image, fileSize);
 
-    std::cout << "image size:" << fileSize << std::endl;
     proto.mutable_tensor_shape()->add_dim()->set_size(1);
 
     inputs["images"] = proto;
     
     Status status = stub_->Predict(&context, predictRequest, &response);
+
+    delete[] image;
+
     if (status.ok()) {
       std::cout << "call predict ok" << std::endl;
       std::cout << "outputs size is "<< response.outputs_size() << std::endl;
       OutMap& map_outputs =  *response.mutable_outputs();
       OutMap::iterator iter;
       int output_index = 0;
+      
       for(iter = map_outputs.begin();iter != map_outputs.end(); ++iter){
-        std::cout << "output " << output_index << " name is "<< iter->first << std::endl;
-
-        std::cout << "output " << output_index << " type is "<< 
-            ::tensorflow::DataType_Name(
-              iter->second.dtype()) << std::endl;
-
-        int value_size =  0;
-        switch(iter->second.dtype()){
-          case ::tensorflow::DataType::DT_STRING:
-            value_size = iter->second.string_val_size();
-            break;
-          case ::tensorflow::DataType::DT_FLOAT:
-            value_size = iter->second.float_val_size();
-            break;
-          default:
-            break;
-        }
-        std::cout << "outputs has "<< value_size << " value(s)" ;
-
-        std::cout << " and shaped as [ " ;
-        int dim_size = iter->second.tensor_shape().dim_size();
-        ::google::protobuf::int64 * dims = 
-            new ::google::protobuf::int64 [dim_size];
-        for(int d=0; d < dim_size; ++d){
-          dims[d] =  iter->second.tensor_shape().dim(d).size();
-          std::cout << dims[d] << " ";
-        }
-        std::cout << "]" << std::endl;
-
-        for(int k=0; k < value_size; ++k) {
-          switch(iter->second.dtype()){
-            case ::tensorflow::DataType::DT_STRING:
-              std::cout << "value at " << k << " is :" << iter->second.string_val(k) << std::endl;
-              break;
-            case ::tensorflow::DataType::DT_FLOAT:
-              std::cout << "value at " << k << " is :" << iter->second.float_val(k) << std::endl;
-              break;
-            default:
-              break;
-          }
-        }  
-         
-        if(dims) {
-          delete[] dims;
-          dims = NULL;
+        tensorflow::TensorProto& result_tensor_proto= iter->second;
+        tensorflow::Tensor tensor;
+        bool converted = tensor.FromProto(result_tensor_proto);
+        if (converted) {
+          std::cout << "the result tensor[" << output_index << "] is:" <<
+               std::endl << tensor.SummarizeValue(10) << std::endl;
+        }else {
+          std::cout << "the result tensor[" << output_index << 
+               "] convert failed." << std::endl;
         }
         ++output_index;
       }
-      if(image) {
-        delete[] image;
-        image = NULL;
-      }
-      return "ok";
+      return "Done.";
     } else {
-      std::cout << status.error_code() << ": " << status.error_message()
-                << std::endl;
-      if(image) {
-        delete[] image;
-        image = NULL;
-      }
-      return "RPC failed";
+      std::cout << "gRPC call return code: " 
+          <<status.error_code() << ": " << status.error_message()
+          << std::endl;
+      return "gRPC failed.";
     }
   }
 
@@ -145,35 +100,30 @@ class ServingClient {
   std::unique_ptr<PredictionService::Stub> stub_;
 };
 
-
-std::string GetImagePath(int argc, char** argv) {
-  std::string image_path;
-  std::string arg_str("--image");
-  if (argc > 1) {
-    std::string argv_1 = argv[1];
-    size_t start_position = argv_1.find(arg_str);
-    if (start_position != std::string::npos) {
-      start_position += arg_str.size();
-      if (argv_1[start_position] == ' ' ||
-          argv_1[start_position] == '=') {
-        image_path = argv_1.substr(start_position + 1);
-      }
-    }
-  } else {
-    image_path = INPUT_FILE_NAME;
-  }
-  return image_path;
-}
-
-
 int main(int argc, char** argv) {
-  // Expect only arg: --db_path=path/to/route_guide_db.json.
-  std::string image_path = GetImagePath(argc, argv);
+  std::string server_port = "localhost:9000";
+  std::string image_file = "";
+  std::string model_name = "inception";
+  std::vector<tensorflow::Flag> flag_list = {
+      tensorflow::Flag("server_port", &server_port, 
+          "the IP and port of the server"),
+      tensorflow::Flag("image_file", &image_file, 
+          "the path to the "),
+      tensorflow::Flag("model_name", &model_name, "name of model")
+  };
+  std::string usage = tensorflow::Flags::Usage(argv[0], flag_list);
+  const bool parse_result = tensorflow::Flags::Parse(&argc, argv, flag_list);
+  if (!parse_result || image_file.empty()) {
+    std::cout << usage;
+    return -1;
+  }
+
   ServingClient guide(
-      grpc::CreateChannel( SERVING_HOST,
+      grpc::CreateChannel( server_port,
                           grpc::InsecureChannelCredentials()));
-  std::cout << "calling predict using file: " << image_path << "  ..." << std::endl;
-  std::cout << guide.callPredict(image_path) << std::endl;
+  std::cout << "calling predict using file: " << 
+      image_file << "  ..." << std::endl;
+  std::cout << guide.callPredict(model_name, image_file) << std::endl;
 
   return 0;
 }
