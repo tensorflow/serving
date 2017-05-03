a99ecb8cc677f531aa8b6e916a7432b12c7090cd
Kiril Gorovoy <kgorovoy@google.com>
2017-05-02 17:36:46 -0700

    Add license string to inception_client.cc and make some modifications to make code compatible with Google style.

--- tensorflow_serving/example/inception_client.cc
+++ tensorflow_serving/example/inception_client.cc
@@ -1,40 +1,53 @@
-#include <iostream>
-#include <fstream>
+/* Copyright 2017 Google Inc. All Rights Reserved.
 
-#include <grpc++/create_channel.h>
-#include "tensorflow_serving/apis/prediction_service.grpc.pb.h"
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#include <fstream>
+#include <iostream>
+
+#include "grpc++/create_channel.h"
+#include "grpc++/security/credentials.h"
+#include "google/protobuf/map.h"
 #include "tensorflow/core/framework/tensor.h"
+#include "tensorflow/core/platform/types.h"
 #include "tensorflow/core/util/command_line_flags.h"
+#include "tensorflow_serving/apis/prediction_service.grpc.pb.h"
 
 using grpc::Channel;
 using grpc::ClientContext;
-using grpc::ClientReader;
-using grpc::ClientReaderWriter;
-using grpc::ClientWriter;
 using grpc::Status;
-
 
 using tensorflow::serving::PredictRequest;
 using tensorflow::serving::PredictResponse;
 using tensorflow::serving::PredictionService;
 
-typedef google::protobuf::Map< std::string, tensorflow::TensorProto > OutMap;
-
+typedef google::protobuf::Map<tensorflow::string, tensorflow::TensorProto> OutMap;
 
 class ServingClient {
  public:
   ServingClient(std::shared_ptr<Channel> channel)
-      : stub_(PredictionService::NewStub(channel)) {
-  }
+      : stub_(PredictionService::NewStub(channel)) {}
 
-  std::string callPredict(std::string model_name, std::string file_path){
+  tensorflow::string callPredict(const tensorflow::string& model_name,
+                                 const tensorflow::string& file_path) {
     PredictRequest predictRequest;
     PredictResponse response;
     ClientContext context;
 
     predictRequest.mutable_model_spec()->set_name(model_name);
 
-    google::protobuf::Map< std::string, tensorflow::TensorProto >& inputs = 
+    google::protobuf::Map<tensorflow::string, tensorflow::TensorProto>& inputs =
         *predictRequest.mutable_inputs();
 
     tensorflow::TensorProto proto;
@@ -46,15 +59,14 @@ class ServingClient {
       return "";
     }
 
-    std::filebuf * pbuf = imageFile.rdbuf();
-    long fileSize = pbuf->pubseekoff(0, std::ios::end, std::ios::in);
-    
+    std::filebuf* pbuf = imageFile.rdbuf();
+    auto fileSize = pbuf->pubseekoff(0, std::ios::end, std::ios::in);
+
     char* image = new char[fileSize]();
 
     pbuf->pubseekpos(0, std::ios::in);
     pbuf->sgetn(image, fileSize);
     imageFile.close();
-    
 
     proto.set_dtype(tensorflow::DataType::DT_STRING);
     proto.add_string_val(image, fileSize);
@@ -62,36 +74,36 @@ class ServingClient {
     proto.mutable_tensor_shape()->add_dim()->set_size(1);
 
     inputs["images"] = proto;
-    
+
     Status status = stub_->Predict(&context, predictRequest, &response);
 
     delete[] image;
 
     if (status.ok()) {
       std::cout << "call predict ok" << std::endl;
-      std::cout << "outputs size is "<< response.outputs_size() << std::endl;
-      OutMap& map_outputs =  *response.mutable_outputs();
+      std::cout << "outputs size is " << response.outputs_size() << std::endl;
+      OutMap& map_outputs = *response.mutable_outputs();
       OutMap::iterator iter;
       int output_index = 0;
-      
-      for(iter = map_outputs.begin();iter != map_outputs.end(); ++iter){
-        tensorflow::TensorProto& result_tensor_proto= iter->second;
+
+      for (iter = map_outputs.begin(); iter != map_outputs.end(); ++iter) {
+        tensorflow::TensorProto& result_tensor_proto = iter->second;
         tensorflow::Tensor tensor;
         bool converted = tensor.FromProto(result_tensor_proto);
         if (converted) {
-          std::cout << "the result tensor[" << output_index << "] is:" <<
-               std::endl << tensor.SummarizeValue(10) << std::endl;
-        }else {
-          std::cout << "the result tensor[" << output_index << 
-               "] convert failed." << std::endl;
+          std::cout << "the result tensor[" << output_index
+                    << "] is:" << std::endl
+                    << tensor.SummarizeValue(10) << std::endl;
+        } else {
+          std::cout << "the result tensor[" << output_index
+                    << "] convert failed." << std::endl;
         }
         ++output_index;
       }
       return "Done.";
     } else {
-      std::cout << "gRPC call return code: " 
-          <<status.error_code() << ": " << status.error_message()
-          << std::endl;
+      std::cout << "gRPC call return code: " << status.error_code() << ": "
+                << status.error_message() << std::endl;
       return "gRPC failed.";
     }
   }
@@ -101,17 +113,15 @@ class ServingClient {
 };
 
 int main(int argc, char** argv) {
-  std::string server_port = "localhost:9000";
-  std::string image_file = "";
-  std::string model_name = "inception";
+  tensorflow::string server_port = "localhost:9000";
+  tensorflow::string image_file = "";
+  tensorflow::string model_name = "inception";
   std::vector<tensorflow::Flag> flag_list = {
-      tensorflow::Flag("server_port", &server_port, 
-          "the IP and port of the server"),
-      tensorflow::Flag("image_file", &image_file, 
-          "the path to the "),
-      tensorflow::Flag("model_name", &model_name, "name of model")
-  };
-  std::string usage = tensorflow::Flags::Usage(argv[0], flag_list);
+      tensorflow::Flag("server_port", &server_port,
+                       "the IP and port of the server"),
+      tensorflow::Flag("image_file", &image_file, "the path to the "),
+      tensorflow::Flag("model_name", &model_name, "name of model")};
+  tensorflow::string usage = tensorflow::Flags::Usage(argv[0], flag_list);
   const bool parse_result = tensorflow::Flags::Parse(&argc, argv, flag_list);
   if (!parse_result || image_file.empty()) {
     std::cout << usage;
@@ -119,10 +129,9 @@ int main(int argc, char** argv) {
   }
 
   ServingClient guide(
-      grpc::CreateChannel( server_port,
-                          grpc::InsecureChannelCredentials()));
-  std::cout << "calling predict using file: " << 
-      image_file << "  ..." << std::endl;
+      grpc::CreateChannel(server_port, grpc::InsecureChannelCredentials()));
+  std::cout << "calling predict using file: " << image_file << "  ..."
+            << std::endl;
   std::cout << guide.callPredict(model_name, image_file) << std::endl;
 
   return 0;
