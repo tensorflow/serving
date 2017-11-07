5a58ec6220e53662d77738977c42f56167d758f9
Rahul <rahulthusathsuresh@hotmail.com>
2017-11-06 17:17:34 -0800

    Commiting changes for inception Sample Code

--- tensorflow_serving/example/inception_client.py
+++ tensorflow_serving/example/inception_client.py
@@ -1,22 +1,4 @@
-# Copyright 2016 Google Inc. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-
 #!/usr/bin/env python2.7
-
-"""Send JPEG image to tensorflow_model_server loaded with inception model.
-"""
 
 from __future__ import print_function
 
@@ -27,30 +9,33 @@ import tensorflow as tf
 
 from tensorflow_serving.apis import predict_pb2
 from tensorflow_serving.apis import prediction_service_pb2
+import base64
+import os
 
+class Processor:
+  __stub = None
+  __vocab = None
+  __wrap = None
 
-tf.app.flags.DEFINE_string('server', 'localhost:9000',
-                           'PredictionService host:port')
-tf.app.flags.DEFINE_string('image', '', 'path to image in JPEG format')
-FLAGS = tf.app.flags.FLAGS
+  def __init__(self, server, dataDir):
+    host, port = server.split(':')
+    channel = implementations.insecure_channel(host, int(port))
+    self.__stub = prediction_service_pb2.beta_create_PredictionService_stub(channel)
 
-
-def main(_):
-  host, port = FLAGS.server.split(':')
-  channel = implementations.insecure_channel(host, int(port))
-  stub = prediction_service_pb2.beta_create_PredictionService_stub(channel)
-  # Send request
-  with open(FLAGS.image, 'rb') as f:
-    # See prediction_service.proto for gRPC request/response details.
-    data = f.read()
+  def Process(self, modelName, inputList):
+    image = base64.decodestring(inputList[0])
+    # Send request
     request = predict_pb2.PredictRequest()
-    request.model_spec.name = 'inception'
+    request.model_spec.name = modelName
     request.model_spec.signature_name = 'predict_images'
     request.inputs['images'].CopyFrom(
-        tf.contrib.util.make_tensor_proto(data, shape=[1]))
-    result = stub.Predict(request, 10.0)  # 10 secs timeout
+      tf.contrib.util.make_tensor_proto(image, shape=[1]))
+    result = self.__stub.Predict(request, 10.0)  # 10 secs timeout
     print(result)
+    myresult = str(result)
+    return myresult
 
 
 if __name__ == '__main__':
   tf.app.run()
+
