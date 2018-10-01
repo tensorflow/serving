df5ad1540993bda08303436f9b72d8b2547ff1e3
gvasudevan <gvasudevan@google.com>
2018-10-01 16:05:29 -0700

    Default to valid version for wheel file
    PiperOrigin-RevId: 215297715

--- tensorflow_serving/tools/pip_package/setup.py
+++ tensorflow_serving/tools/pip_package/setup.py
@@ -29,7 +29,7 @@ from setuptools import setup
 DOCLINES = __doc__.split('\n')
 
 # Set when releasing a new version of TensorFlow Serving (e.g. 1.0.0).
-_VERSION = 'undefined'
+_VERSION = '0.0.0'
 
 REQUIRED_PACKAGES = [
     'tensorflow>=1.2.0,<2',
