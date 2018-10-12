c0960079984732441c63027e2ec5f00f14530574
gvasudevan <gvasudevan@google.com>
2018-10-11 19:10:30 -0700

    Update comments in ResNet client script
    PiperOrigin-RevId: 216799189

--- tensorflow_serving/example/resnet_client.py
+++ tensorflow_serving/example/resnet_client.py
@@ -17,6 +17,13 @@
 The client downloads a test image of a cat, queries the server over the REST API
 with the test image repeatedly and measures how long it takes to respond.
 
+The client expects a TensorFlow Serving ModelServer running a ResNet SavedModel
+from:
+
+https://github.com/tensorflow/models/tree/master/official/resnet#pre-trained-model
+
+The SavedModel must be one that can take JPEG images as inputs.
+
 Typical usage example:
 
     resnet_client.py
@@ -27,7 +34,11 @@ from __future__ import print_function
 import base64
 import requests
 
+# The server URL specifies the endpoint of your server running the ResNet
+# model with the name "resnet" and using the predict interface.
 SERVER_URL = 'http://localhost:8501/v1/models/resnet:predict'
+
+# The image URL is the location of the image we should send to the server
 IMAGE_URL = 'https://tensorflow.org/images/blogs/serving/cat.jpg'
 
 
