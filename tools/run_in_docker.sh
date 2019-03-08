dd59021d3f807f23390afa8a2bc34a6f7029ed24
awk <awk@google.com>
2019-03-08 15:32:58 -0800

    Add -o option, to pass params to `docker` command.
    PiperOrigin-RevId: 237531813

--- tools/run_in_docker.sh
+++ tools/run_in_docker.sh
@@ -39,7 +39,7 @@ set -e
 function usage() {
   local progname=$(basename $0)
   echo "Usage:"
-  echo "  ${progname} [-d <docker-image-name>] <command> [args ...]"
+  echo "  ${progname} [-d <docker-image-name>] [-o <docker-run-options>] <command> [args ...]"
   echo ""
   echo "Examples:"
   echo "  ${progname} bazel build tensorflow_serving/model_servers:tensorflow_model_server"
@@ -68,13 +68,21 @@ function get_python_cmd() {
 }
 
 (( $# < 1 )) && usage
-[[ "$1" = "-"* ]] && [[ "$1" != "-d" ]] && usage
 
 IMAGE="tensorflow/serving:nightly-devel"
-[[ "$1" = "-d" ]] && IMAGE=$2 && shift 2 || true
-[[ "${IMAGE}" = "" ]] && usage
+RUN_OPTS=()
+while [[ $# > 1 ]]; do
+  case "$1" in
+    -d)
+      IMAGE="$2"; shift 2;;
+    -o)
+      RUN_OPTS=($2); shift 2;;
+    *)
+      break;;
+  esac
+done
 
-RUN_OPTS=(--rm -it --network=host)
+RUN_OPTS+=(--rm -it --network=host)
 # Map the working directory and /tmp to allow scripts/binaries to run and also
 # output data that might be used by other scripts/binaries
 RUN_OPTS+=("-v $(pwd):$(pwd)")
