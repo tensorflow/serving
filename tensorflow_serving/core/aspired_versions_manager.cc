e384bf406a5411a0539973c4c3aa13f0f889ebb3
A. Unique TensorFlower <gardener@tensorflow.org>
2019-03-12 12:12:53 -0700

    Add servable_name to debugging log.
    PiperOrigin-RevId: 238068691

--- tensorflow_serving/core/aspired_versions_manager.cc
+++ tensorflow_serving/core/aspired_versions_manager.cc
@@ -238,7 +238,7 @@ void AspiredVersionsManager::EnqueueAspiredVersionsRequest(
 
   {
     mutex_lock l(pending_aspired_versions_requests_mu_);
-    VLOG(1) << "Enqueueing aspired versions request: "
+    VLOG(1) << "Enqueueing aspired versions request: " << servable_name << ": "
             << ServableVersionsDebugString(versions);
     pending_aspired_versions_requests_[string(servable_name)] =
         std::move(versions);
@@ -248,7 +248,7 @@ void AspiredVersionsManager::EnqueueAspiredVersionsRequest(
 void AspiredVersionsManager::ProcessAspiredVersionsRequest(
     const StringPiece servable_name,
     std::vector<ServableData<std::unique_ptr<Loader>>> versions) {
-  VLOG(1) << "Processing aspired versions request: "
+  VLOG(1) << "Processing aspired versions request: " << servable_name << ": "
           << ServableVersionsDebugString(versions);
 
   const std::set<int64> next_aspired_versions = GetVersionNumbers(versions);
