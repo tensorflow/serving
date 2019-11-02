3ea69f4cf1c69836e7574d7938e524ea6ed2c4d2
Ben Hoff <hoff.benjamin.k@gmail.com>
2019-11-02 11:05:17 -0400

    remove obsolete method call from python example

--- tensorflow_serving/example/mnist_client.py
+++ tensorflow_serving/example/mnist_client.py
@@ -146,7 +146,7 @@ def do_inference(hostport, work_dir, concurrency, num_tests):
     request.model_spec.signature_name = 'predict_images'
     image, label = test_data_set.next_batch(1)
     request.inputs['images'].CopyFrom(
-        tf.contrib.util.make_tensor_proto(image[0], shape=[1, image[0].size]))
+        tf.make_tensor_proto(image[0], shape=[1, image[0].size]))
     result_counter.throttle()
     result_future = stub.Predict.future(request, 5.0)  # 5 seconds
     result_future.add_done_callback(
