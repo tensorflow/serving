22635f61110045f27c433a5fcf336138f86df4a6
tensorflow-copybara <tensorflow.copybara@gmail.com>
2020-05-19 11:10:25 -0700

    Merge pull request #1615 from peakji:master
    PiperOrigin-RevId: 312314660
    (cherry picked from commit fb167400cab14600c86765b6415da164c4a3e0aa)

--- tensorflow_serving/util/json_tensor_test.cc
+++ tensorflow_serving/util/json_tensor_test.cc
@@ -1017,6 +1017,80 @@ TEST(JsontensorTest, FromJsonMultipleNamedTensorsErrors) {
   EXPECT_THAT(status.error_message(), HasSubstr("inconsistent batch size"));
 }
 
+TEST(JsontensorTest, FromJsonSingleZeroBatchTensor) {
+  TensorMap tensormap;
+  ASSERT_TRUE(TextFormat::ParseFromString(R"(
+    dtype: DT_INT32
+    tensor_shape {
+      dim { size: 0 }
+      dim { size: 2 }
+    }
+    )",
+                                          &tensormap["int_tensor"]));
+
+  string json;
+  TF_EXPECT_OK(
+      MakeJsonFromTensors(tensormap, JsonPredictRequestFormat::kRow, &json));
+  TF_EXPECT_OK(CompareJson(json, R"({ "predictions": [] })"));
+}
+
+TEST(JsontensorTest, FromJsonMultipleZeroBatchTensors) {
+  TensorMap tensormap;
+  ASSERT_TRUE(TextFormat::ParseFromString(R"(
+    dtype: DT_STRING
+    tensor_shape {
+      dim { size: 0 }
+      dim { size: 1 }
+      dim { size: 2 }
+    }
+    )",
+                                          &tensormap["str_tensor"]));
+
+  ASSERT_TRUE(TextFormat::ParseFromString(R"(
+    dtype: DT_FLOAT
+    tensor_shape {
+      dim { size: 0 }
+      dim { size: 3 }
+      dim { size: 4 }
+    }
+    )",
+                                          &tensormap["float_tensor"]));
+
+  string json;
+  TF_EXPECT_OK(
+      MakeJsonFromTensors(tensormap, JsonPredictRequestFormat::kRow, &json));
+  TF_EXPECT_OK(CompareJson(json, R"({ "predictions": [] })"));
+}
+
+TEST(JsontensorTest, FromJsonMultipleZeroBatchTensorsErrors) {
+  TensorMap tensormap;
+  ASSERT_TRUE(TextFormat::ParseFromString(R"(
+    dtype: DT_INT32
+    tensor_shape {
+      dim { size: 0 }
+      dim { size: 2 }
+    }
+    )",
+                                          &tensormap["int_tensor"]));
+
+  ASSERT_TRUE(TextFormat::ParseFromString(R"(
+    dtype: DT_STRING
+    tensor_shape {
+      dim { size: 1 }
+      dim { size: 2 }
+    }
+    string_val: "foo"
+    string_val: "bar"
+    )",
+                                          &tensormap["str_tensor"]));
+
+  string json;
+  const auto& status =
+      MakeJsonFromTensors(tensormap, JsonPredictRequestFormat::kRow, &json);
+  ASSERT_TRUE(errors::IsInvalidArgument(status));
+  EXPECT_THAT(status.error_message(), HasSubstr("inconsistent batch size"));
+}
+
 template <typename RequestType>
 class ClassifyRegressRequestTest : public ::testing::Test {
  protected:
