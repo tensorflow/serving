c1ce0752f1076bd6f92e1af5f73e3a3c552f4691
Abhijit Karmarkar <awk@google.com>
2020-05-21 12:56:44 -0700

    Fix broken GetModelMetadata request processing.
    Fixes #1612
    
    PiperOrigin-RevId: 312723939

--- tensorflow_serving/model_servers/prediction_service_impl.cc
+++ tensorflow_serving/model_servers/prediction_service_impl.cc
@@ -69,11 +69,6 @@ thread::ThreadPoolOptions GetThreadPoolOptions(
 ::grpc::Status PredictionServiceImpl::GetModelMetadata(
     ::grpc::ServerContext *context, const GetModelMetadataRequest *request,
     GetModelMetadataResponse *response) {
-  if (!use_saved_model_) {
-    return ToGRPCStatus(
-        errors::InvalidArgument("GetModelMetadata API is only available when "
-                                "use_saved_model is set to true"));
-  }
   const ::grpc::Status status = ToGRPCStatus(
       GetModelMetadataImpl::GetModelMetadata(core_, *request, response));
   if (!status.ok()) {
