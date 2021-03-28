88c29c4e31a1882381b7b21bfd3d180f2df989c9
kiddos <kiddo831007@gmail.com>
2021-03-28 23:44:35 +0800

    add notes that OPTIONS request is not allowed by default in libevent2

--- tensorflow_serving/util/net_http/server/internal/evhttp_server.cc
+++ tensorflow_serving/util/net_http/server/internal/evhttp_server.cc
@@ -105,6 +105,10 @@ bool EvHTTPServer::Initialize() {
     return false;
   }
 
+  // By default libevents only allow GET, POST, HEAD, PUT, DELETE request
+  // we have to manually turn OPTIONS flag on
+  // documentation:
+  // (http://www.wangafu.net/~nickm/libevent-2.0/doxygen/html/http_8h.html)
   evhttp_set_allowed_methods(ev_http_,
                              EVHTTP_REQ_GET | EVHTTP_REQ_POST | EVHTTP_REQ_HEAD |
                              EVHTTP_REQ_PUT | EVHTTP_REQ_DELETE | EVHTTP_REQ_OPTIONS);
