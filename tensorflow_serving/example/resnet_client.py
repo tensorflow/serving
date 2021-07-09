cb28fcab721b366c75f974ad6eead53659ba00d7
A. Unique TensorFlower <gardener@tensorflow.org>
2021-07-08 18:37:12 -0700

    Update resnet_client.py example
    PiperOrigin-RevId: 383755518

--- tensorflow_serving/example/resnet_client.py
+++ tensorflow_serving/example/resnet_client.py
@@ -20,9 +20,7 @@ with the test image repeatedly and measures how long it takes to respond.
 The client expects a TensorFlow Serving ModelServer running a ResNet SavedModel
 from:
 
-https://github.com/tensorflow/models/tree/master/official/resnet#pre-trained-model
-
-The SavedModel must be one that can take JPEG images as inputs.
+https://github.com/tensorflow/models/tree/master/official/vision/image_classification/resnet#pretrained-models
 
 Typical usage example:
 
@@ -32,6 +30,11 @@ Typical usage example:
 from __future__ import print_function
 
 import base64
+import io
+import json
+
+import numpy as np
+from PIL import Image
 import requests
 
 # The server URL specifies the endpoint of your server running the ResNet
@@ -41,15 +44,26 @@ SERVER_URL = 'http://localhost:8501/v1/models/resnet:predict'
 # The image URL is the location of the image we should send to the server
 IMAGE_URL = 'https://tensorflow.org/images/blogs/serving/cat.jpg'
 
+# Current Resnet model in TF Model Garden (as of 7/2021) does not accept JPEG
+# as input
+MODEL_ACCEPT_JPG = False
+
 
 def main():
   # Download the image
   dl_request = requests.get(IMAGE_URL, stream=True)
   dl_request.raise_for_status()
 
-  # Compose a JSON Predict request (send JPEG image in base64).
-  jpeg_bytes = base64.b64encode(dl_request.content).decode('utf-8')
-  predict_request = '{"instances" : [{"b64": "%s"}]}' % jpeg_bytes
+  if MODEL_ACCEPT_JPG:
+    # Compose a JSON Predict request (send JPEG image in base64).
+    jpeg_bytes = base64.b64encode(dl_request.content).decode('utf-8')
+    predict_request = '{"instances" : [{"b64": "%s"}]}' % jpeg_bytes
+  else:
+    # Compose a JOSN Predict request (send the image tensor).
+    jpeg_rgb = Image.open(io.BytesIO(dl_request.content))
+    # Normalize and batchify the image
+    jpeg_rgb = np.expand_dims(np.array(jpeg_rgb) / 255.0, 0).tolist()
+    predict_request = json.dumps({'instances': jpeg_rgb})
 
   # Send few requests to warm-up the model.
   for _ in range(3):
@@ -66,7 +80,7 @@ def main():
     prediction = response.json()['predictions'][0]
 
   print('Prediction class: {}, avg latency: {} ms'.format(
-      prediction['classes'], (total_time*1000)/num_requests))
+      np.argmax(prediction), (total_time * 1000) / num_requests))
 
 
 if __name__ == '__main__':
