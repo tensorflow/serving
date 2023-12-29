2b9e58cf4ad222adaa741d0cdac3b5a0d220b8fa
A. Unique TensorFlower <gardener@tensorflow.org>
2023-12-28 18:23:10 -0800

    Move TPU runner init stub to tensorflow serving OSS directory.
    PiperOrigin-RevId: 594346880

---
+++ tensorflow_serving/model_servers/device_runner_init_stub.cc
@@ -0,0 +1,34 @@
+/* Copyright 2021 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+#include "tensorflow_serving/model_servers/device_runner_init_stub.h"
+
+#include "absl/base/attributes.h"
+#include "tensorflow/core/platform/errors.h"
+#include "tensorflow/core/platform/status.h"
+#include "tensorflow/core/tfrt/runtime/runtime.h"
+
+namespace tensorflow::serving {
+
+namespace {
+Status InitializeDeviceRunnerAndTopologyStub(tfrt_stub::Runtime&, int*, int*) {
+  return tensorflow::errors::Internal(
+      "InitializeTpuRunner is not linked into this binary");
+}
+}  // namespace
+
+ABSL_CONST_INIT InitializeDeviceRunnerAndTopologyFuncType
+    InitializeDeviceRunnerAndTopology = InitializeDeviceRunnerAndTopologyStub;
+
+}  // namespace tensorflow::serving
