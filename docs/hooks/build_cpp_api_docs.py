f62049a4ea6428bebcc6d678c284df9df56fdfcb
pdmurray <peynmurray@gmail.com>
2024-10-16 10:56:13 -0700

    Add a splash page for the cpp docs; clean up hook; add gitignore

--- docs/hooks/build_cpp_api_docs.py
+++ docs/hooks/build_cpp_api_docs.py
@@ -1,15 +1,14 @@
+from pathlib import Path
 from shutil import which
 from subprocess import run
-from pathlib import Path
-from mkdocs.structure.files import File
 
+from mkdocs.structure.files import File
 
 doxygen_path = Path(which("doxygen"))
 if not doxygen_path:
     raise FileNotFoundError("`doxygen` not found")
 
 doxyfile_path = Path.cwd() / "Doxyfile"
-print(doxyfile_path)
 if not doxyfile_path.is_file():
     raise FileNotFoundError("`Doxyfile` not found")
 
@@ -29,13 +28,14 @@ def on_pre_build(config):
 def on_files(files, config):
     for subdir in ("", "search"):
         for doc_file in (src_dir / subdir).iterdir():
-            if not doc_file.is_file():
-                continue
-            f = File(
-                path=doc_file.name,
-                src_dir=src_dir / subdir,
-                dest_dir=Path(config.site_dir) / dest_dir / subdir,
-                use_directory_urls=config.use_directory_urls,
-            )
-            files.append(f)
+            if doc_file.is_file():
+                files.append(
+                    File(
+                        path=doc_file.name,
+                        src_dir=src_dir / subdir,
+                        dest_dir=Path(config.site_dir) / dest_dir / subdir,
+                        use_directory_urls=config.use_directory_urls,
+                    )
+                )
+
     return files
