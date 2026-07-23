621c598af67b93e143a4849d555a641db24f9728
Venkata Sai Madhur Karampudi <vkarampudi@google.com>
2026-07-23 08:30:30 +0000

    Remove #include <stdint.h> from cuda_bf16_fix.h to prevent sysroot header pre-include conflicts under Bazel

--- third_party/cuda_bf16_fix.h
+++ third_party/cuda_bf16_fix.h
@@ -2,11 +2,9 @@
 #ifndef THIRD_PARTY_CUDA_BF16_FIX_H_
 #define THIRD_PARTY_CUDA_BF16_FIX_H_
 
-#include <stdint.h>
-
 #if !defined(__clang__) && defined(__GNUC__)
 #ifndef __bf16
-typedef uint16_t __bf16;
+typedef unsigned short __bf16;
 #endif
 #endif
 
