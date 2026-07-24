28283295d4399d60601d11e4fd2e918e6e6fc015
Venkata Sai Madhur Karampudi <vkarampudi@google.com>
2026-07-24 03:32:00 +0000

    Guard __bf16 typedef with __is_identifier(__bf16) so Clang 18 uses its native __bf16 keyword while GCC 10 receives typedef unsigned short __bf16

--- tensorflow_serving/cuda_bf16_fix.h
+++ tensorflow_serving/cuda_bf16_fix.h
@@ -1,6 +1,18 @@
 #ifndef CUDA_BF16_FIX_H_
 #define CUDA_BF16_FIX_H_
-#ifndef __bf16
+
+#if defined(__clang__)
+#  if defined(__is_identifier)
+#    if __is_identifier(__bf16)
+#      ifndef __bf16
 typedef unsigned short __bf16;
+#      endif
+#    endif
+#  endif
+#else
+#  ifndef __bf16
+typedef unsigned short __bf16;
+#  endif
 #endif
-#endif
+
+#endif  // CUDA_BF16_FIX_H_
