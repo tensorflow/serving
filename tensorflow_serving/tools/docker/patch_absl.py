ce1e414b539bcd89d0f784b09a1a103a5b3d22dc
Venkata Sai Madhur Karampudi <vkarampudi@google.com>
2026-07-27 06:34:28 +0000

    Add idempotency checks to patch_absl.py and workspace.bzl patch_cmds

--- tensorflow_serving/tools/docker/patch_absl.py
+++ tensorflow_serving/tools/docker/patch_absl.py
@@ -4,16 +4,22 @@ import sys
 
 def patch_btree(p):
     s = open(p).read()
-    s = re.sub(r"(class btree_iterator : private btree_iterator_generation_info \{)", r"\1\n  template <typename, typename, typename> friend class btree_iterator;", s)
-    s = re.sub(r"(bool operator==\(const const_iterator &other\) const \{)", r"template <typename N, typename R, typename P> bool operator==(const btree_iterator<N, R, P> &other) const { return node_ == other.node_ && position_ == other.position_; }\n  \1", s)
-    s = re.sub(r"(bool operator!=\(const const_iterator &other\) const \{)", r"template <typename N, typename R, typename P> bool operator!=(const btree_iterator<N, R, P> &other) const { return node_ != other.node_ || position_ != other.position_; }\n  \1", s)
-    s = re.sub(r"(const_iterator internal_end\(const_iterator iter\) const \{)", r"const_iterator internal_end(iterator iter) const { return iter.node_ != nullptr ? iter : end(); }\n  \1", s)
-    s = re.sub(r"using iterator\s*=\s*typename btree_iterator<node_type, reference, pointer>::iterator;", r"using iterator = btree_iterator<node_type, reference, pointer>;", s)
+    if "friend class btree_iterator;" not in s:
+        s = re.sub(r"(class btree_iterator : private btree_iterator_generation_info \{)", r"\1\n  template <typename, typename, typename> friend class btree_iterator;", s)
+    if "bool operator==(const btree_iterator<N, R, P>" not in s:
+        s = re.sub(r"(bool operator==\(const const_iterator &other\) const \{)", r"template <typename N, typename R, typename P> bool operator==(const btree_iterator<N, R, P> &other) const { return node_ == other.node_ && position_ == other.position_; }\n  \1", s)
+    if "bool operator!=(const btree_iterator<N, R, P>" not in s:
+        s = re.sub(r"(bool operator!=\(const const_iterator &other\) const \{)", r"template <typename N, typename R, typename P> bool operator!=(const btree_iterator<N, R, P> &other) const { return node_ != other.node_ || position_ != other.position_; }\n  \1", s)
+    if "internal_end(iterator iter) const" not in s:
+        s = re.sub(r"(const_iterator internal_end\(const_iterator iter\) const \{)", r"const_iterator internal_end(iterator iter) const { return iter.node_ != nullptr ? iter : end(); }\n  \1", s)
+    if "using iterator = btree_iterator<node_type, reference, pointer>;" not in s:
+        s = re.sub(r"using iterator\s*=\s*typename btree_iterator<node_type, reference, pointer>::iterator;", r"using iterator = btree_iterator<node_type, reference, pointer>;", s)
     open(p, "w").write(s)
 
 def patch_container(p):
     s = open(p).read()
-    s = re.sub(r"\s*protected:\s*(// Alias used for heterogeneous lookup functions\.)", r"\n public:\n  \1", s)
+    if " public:\n  // Alias used for heterogeneous lookup functions." not in s:
+        s = re.sub(r"\s*protected:\s*(// Alias used for heterogeneous lookup functions\.)", r"\n public:\n  \1", s)
     open(p, "w").write(s)
 
 if __name__ == "__main__":
